461e50c3a0d2d032a86cf9c065430f1a96b2b4fd
Bernardo Nilson <bernardonilson360@gmail.com>
2024-02-20 16:53:55 -0300

    final project

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,27 @@
-
 const pokeApi = {}
 
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+function convertPokeApiDetailToPokemon (pokeDetails) {
+    return new Pokemon(
+        pokeDetails.id,
+        pokeDetails.name,
+        pokeDetails.types.map(typeSlot => typeSlot.type.name),
+        pokeDetails.sprites.other.dream_world.front_default
+    );
+};
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
-}
-
-pokeApi.getPokemonDetail = (pokemon) => {
+pokeApi.getPokemonDetails = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
+        .then(response => response.json())
+        .then(convertPokeApiDetailToPokemon);
+};
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    
     return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
+    .then(response => response.json()) // Conversão da requisição para JSON
+    .then(jsonBody => jsonBody.results) // Focamos somente nos resultados, a lista de pokemons
+    .then(pokemons => pokemons.map(pokemon => pokeApi.getPokemonDetails(pokemon))) // Transformamos a lista de pokemons em uma lista de requisições com os detalhes dos pokemons
+    .then(detailRequest => Promise.all(detailRequest)) // Esperamos todas as requisições terminarem. Retorno da função
+    .catch(error => console.log(error));
+};
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,15 @@
-
 class Pokemon {
     number;
     name;
     type;
     types = [];
     photo;
-}
+
+    constructor (number, name, types, photo) {
+        this.number = number;
+        this.name = name;
+        this.type = types[0]; // Convert single value to array if it's not already
+        this.types = types;
+        this.photo = photo;
+    };
+};
